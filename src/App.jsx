dd328c30e489121b0b22a6bd9bce10eeeaaeb569
Andrew Startsev <andrewstartsev@hotmail.com>
2025-09-23 19:25:32 -0600

    A3 + Prod List

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    A2
+    A3
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
