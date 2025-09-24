9ac77d27821af553531ddafc7a46ecb80245f89e
Andrew Startsev <andrewstartsev@hotmail.com>
2025-09-23 19:17:26 -0600

    A2 store ref

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    A1
+    A2
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
